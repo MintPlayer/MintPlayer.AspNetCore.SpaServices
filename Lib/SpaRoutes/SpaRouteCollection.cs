4a15bbcbcf384822bfc29aa78dd41a7082b3ee6e
MusicDemons <pieterjandeclippel@hotmail.com>
2020-04-28 08:17:21 +0200

    Added sparoutecollection

---
+++ Lib/SpaRoutes/SpaRouteCollection.cs
@@ -0,0 +1,15 @@
+﻿using Spa.SpaRoutes.Data;
+using System.Collections.Generic;
+
+namespace SpaRoutes
+{
+    public interface ISpaRouteCollection : IList<ISpaRouteItem>
+    {
+
+    }
+
+    internal class SpaRouteCollection : List<ISpaRouteItem>, ISpaRouteCollection
+    {
+
+    }
+}
