a52ea551be208d5ae9df9f629e20e753fb3b7086
Pieterjan <pieterjandeclippel@msn.com>
2023-08-18 15:01:44 +0200

    Update angular (#44)
    * angular 15
    
    * 16 upgrade

--- Demo/Demo.Web/ClientApp/src/main.server.ts
+++ Demo/Demo.Web/ClientApp/src/main.server.ts
@@ -9,11 +9,12 @@
 import 'zone.js/dist/zone-node';
 import '@angular/platform-server/init';
 
-import { renderModule, renderModuleFactory } from '@angular/platform-server';
+import { renderModule } from '@angular/platform-server';
 import { APP_BASE_HREF } from '@angular/common';
 import { enableProdMode, StaticProvider, Inject } from '@angular/core';
 import { createServerRenderer, BootFuncParams } from 'aspnet-prerendering';
 import { environment } from './environments/environment';
+import { AppServerModule } from './app/app.server.module';
 
 if (environment.production) {
 	enableProdMode();
@@ -24,7 +25,6 @@ const getBaseUrl = (params: BootFuncParams) => {
 }
 
 export default createServerRenderer(params => {
-	const { AppServerModule, AppServerModuleNgFactory, LAZY_MODULE_MAP } = (module as any).exports;
 
 	const providers: StaticProvider[] = [
 		{ provide: APP_BASE_HREF, useValue: params.baseUrl },
@@ -55,12 +55,10 @@ export default createServerRenderer(params => {
 	// Bypass ssr api call cert warnings in development
 	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
 
-	const renderPromise = AppServerModuleNgFactory
-		? /* AoT */ renderModuleFactory(AppServerModuleNgFactory, options)
-		: /* dev */ renderModule(AppServerModule, options);
+	const renderPromise = renderModule(AppServerModule, options);
 
 	return renderPromise.then(html => ({ html }));
 });
 
-export { renderModule } from '@angular/platform-server';
+
 export { AppServerModule } from './app/app.server.module';
