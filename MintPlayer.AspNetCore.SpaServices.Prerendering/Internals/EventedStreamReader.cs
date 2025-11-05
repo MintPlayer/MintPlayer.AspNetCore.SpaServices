e60e3466dbc676e5be7dc8b8bfc8cea685b37ae4
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-11-05 17:05:17 +0100

    Filter Ansi-coloring from Angular output

--- MintPlayer.AspNetCore.SpaServices.Prerendering/Internals/EventedStreamReader.cs
+++ MintPlayer.AspNetCore.SpaServices.Prerendering/Internals/EventedStreamReader.cs
@@ -7,7 +7,7 @@ namespace MintPlayer.AspNetCore.SpaServices.Prerendering.Internals;
 /// Wraps a <see cref="StreamReader"/> to expose an evented API, issuing notifications
 /// when the stream emits partial lines, completed lines, or finally closes.
 /// </summary>
-internal sealed class EventedStreamReader
+internal sealed partial class EventedStreamReader
 {
 	public delegate void OnReceivedChunkHandler(ArraySegment<char> chunk);
 	public delegate void OnReceivedLineHandler(string line);
@@ -50,7 +50,8 @@ internal sealed class EventedStreamReader
 
 		onReceivedLineHandler = line =>
 		{
-			var match = regex.Match(line);
+			var cleanedLine = AnsiCharacterRegex().Replace(line, string.Empty);
+			var match = regex.Match(cleanedLine);
 			if (match.Success)
 			{
 				ResolveIfStillPending(() => tcs.SetResult(match));
@@ -67,6 +68,9 @@ internal sealed class EventedStreamReader
 
 		return tcs.Task;
 	}
+
+	[GeneratedRegex(@"\x1B\[[0-9;]*[ -/]*[@-~]")]
+	private partial Regex AnsiCharacterRegex();
 
 	private async Task Run()
 	{
